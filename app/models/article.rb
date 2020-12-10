3fb344737f7d8b88a6d9db44ae8664a78b9457c8
AGL9 <a.g.lakshmanan1@gmail.com>
2020-12-10 16:46:19 +0530

    Added comment feature in blog

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}, format: { with: /\A[a-zA-Z]+\z/, message: "Only alphabets" }
+
+  has_many :comments
 end
