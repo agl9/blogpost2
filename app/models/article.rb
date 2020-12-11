c7134ed8e5e6411148dd2929f980e26f4b3c5faf
AGL9 <a.g.lakshmanan1@gmail.com>
2020-12-11 12:39:10 +0530

    added tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,19 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}, format: { with: /\A[a-zA-Z]+\z/, message: "Only alphabets" }
 
   has_many :comments
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(', ')
+  end
+
+  def tag_list= (tags_string)
+    tag_names = tags_string.split(',').collect{|tag| tag.strip.downcase}.uniq
+    new_or_found = tag_names.collect{|name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :tags
+
 end
