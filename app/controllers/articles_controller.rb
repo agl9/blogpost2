b5a1508b4ee5594e8dab4b447e1c80b79fe46fc1
AGL9 <a.g.lakshmanan1@gmail.com>
2020-12-11 12:39:10 +0530

    added tagging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,7 +47,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
